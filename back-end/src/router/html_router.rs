d2f49a1b740fe5fa3f119adbff4143e2e7abfa70
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-01-04 13:13:01 -0700

    chore: swap out proxy service for axum 0.8.1

--- back-end/src/router/html_router.rs
+++ back-end/src/router/html_router.rs
@@ -6,14 +6,13 @@ pub(crate) fn build_html_router() -> Router {
     if cfg!(debug_assertions) {
         event!(Level::INFO, "Serving website via proxy");
         // TODO we'll want to be able to pass this in as an ENV variable
-        let vite_proxy_service_builder =
-            reverse_proxy_service::builder_http("127.0.0.1:4000").unwrap();
+        let vite_proxy_service_builder = axum_proxy::builder_http("127.0.0.1:4000").unwrap();
 
-        let svc: reverse_proxy_service::ReusedService<
-            reverse_proxy_service::Identity,
-            reverse_proxy_service::client::HttpConnector,
+        let svc: axum_proxy::ReusedService<
+            axum_proxy::Identity,
+            axum_proxy::client::HttpConnector,
             axum::body::Body,
-        > = vite_proxy_service_builder.build(reverse_proxy_service::rewrite::Identity {});
+        > = vite_proxy_service_builder.build(axum_proxy::rewrite::Identity {});
 
         Router::new().nest_service("/", svc)
     } else {
